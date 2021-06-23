e39ddbf7880e244384b07c1bec27600405ef6aac
zannatzoya <chandrakant.m18@gmail.com>
2021-06-23 13:47:14 +0530

    Demo Second Commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Demo first");
+		System.out.println("Demo second");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
