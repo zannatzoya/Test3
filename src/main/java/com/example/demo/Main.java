9a2d3b78451957bbc12e6f3cca2be1bbe5a01a57
zannatzoya <chandrakant.m18@gmail.com>
2021-06-23 17:18:08 +0530

    Commit

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,7 +4,11 @@ public class Main {
  
 	public static void main(String[] args) {
 		
+<<<<<<< Upstream, based on origin/master
 		System.out.println("from git11");
 		
+=======
+		System.out.println("from eclipse");
+>>>>>>> 1f94486 from eclipse
 	}
 } 
