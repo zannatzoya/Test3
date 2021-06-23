e89beedc396254c1a10de61d2f11d8de64f6176a
zannatzoya <chandrakant.m18@gmail.com>
2021-06-23 17:15:11 +0530

    from eclipse

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -4,11 +4,7 @@ public class Main {
  
 	public static void main(String[] args) {
 		
-<<<<<<< Upstream, based on origin/master
 		System.out.println("from git11");
-		
-=======
-		System.out.println("from eclipse");
->>>>>>> 1f94486 from eclipse
+		System.out.println("from eclipse"); 
 	}
 } 
