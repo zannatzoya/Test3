26e208a8d4bb0b7b5ad398af66503194bec71b92
zannatzoya <chandrakant.m18@gmail.com>
2021-06-23 15:35:06 +0530

    Changes

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,7 +8,8 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("Demo first");
-		System.out.println("Demo second");
+		System.out.println("Demo second"); 
+		
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
